ae48feacf8c7c9710270f19f8a0cf34a394ff879
voidbert <humbertogilgomes@protonmail.com>
2025-02-07 20:04:43 +0000

    Fix formatting of the test main file

--- src/main.cpp
+++ src/main.cpp
@@ -15,24 +15,24 @@
 /// limitations under the License.
 
 int main(int argc, char **argv) {
-  if (!glfwInit())
-    return -1;
+    if (!glfwInit())
+        return -1;
 
-  /* Create a windowed mode window and its OpenGL context */
-  GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-  if (!window) {
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     glfwTerminate();
-    return -1;
-  }
-
-  glfwMakeContextCurrent(window);
-  while (!glfwWindowShouldClose(window)) {
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-
-  glfwTerminate();
-  return 0;
+    return 0;
 }
