b9ba29a278e377527bb2752d8ac3559e7ef372b7
Mariana <rc.mariana.01@gmail.com>
2025-02-09 01:52:09 +0000

    Added generateSphere

---
+++ src/generator/figures/sphere.cpp
@@ -0,0 +1,50 @@
+/// Copyright 2025 Ana Oliveira, Humberto Gomes, Mariana Rocha, Sara Lopes
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
+#include <glm/glm.hpp>
+#include <math.h>
+#include <string>
+#include <vector>
+
+#include "generator/figures/sphere.hpp"
+
+void generateSphere(float radius, int slices, int stacks, const std::string &file3d) {
+    std::vector<glm::vec4> vertexes;
+    std::vector<std::vector<int>> faces;
+    float stackAngle = M_PI / stacks;
+    float sliceAngle = 2 * M_PI / slices;
+
+    for (int iStack = 0; iStack < stacks; iStack++) {
+        float phi = iStack * stackAngle;
+        float y = radius * cos(phi);
+        float xz = radius * sin(phi);
+
+        for (int jSlice = 0; jSlice < slices; jSlice++) {
+            float theta = jSlice * sliceAngle;
+            float x = xz * cos(theta);
+            float z = xz * sin(theta);
+            vertexes.push_back(glm::vec4(x, y, z, 1.0));
+
+            int firstVertex = iStack * (slices + 1) + jSlice;
+            int secondVertex = firstVertex + slices + 1;
+
+            std::vector<int> triangle1 = { firstVertex, secondVertex, firstVertex + 1 };
+            std::vector<int> triangle2 = { secondVertex, secondVertex + 1, firstVertex + 1 };
+            faces.push_back(triangle1);
+            faces.push_back(triangle2);
+        }
+    }
+
+    // writeObjFile(file3d, std::make_pair(vertexes, faces));
+}
