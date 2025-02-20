e64f2bc58a42dcfd156da2c9d2379d4f587dcfa7
Mariana <rc.mariana.01@gmail.com>
2025-02-20 12:23:45 +0000

    Add Cylinder

--- src/generator/main.cpp
+++ src/generator/main.cpp
@@ -17,6 +17,7 @@
 #include <sstream>
 
 #include "generator/figures/Box.hpp"
+#include "generator/figures/Cylinder.hpp"
 #include "generator/figures/Plane.hpp"
 #include "generator/figures/Sphere.hpp"
 #include "utils/WavefrontOBJ.hpp"
@@ -28,6 +29,8 @@ void printUsage(const std::string &programName) {
     std::cerr << "  " << programName << " box    <length> <grid>      <file>" << std::endl;
     std::cerr << "  " << programName << " sphere <radius> <slices>    <stacks> <file>" << std::endl;
     std::cerr << "  " << programName << " cone   <radius> <height>    <slices> <stacks> <file>"
+              << std::endl;
+    std::cerr << "  " << programName << " cylinder <radius> <height>  <slices> <stacks> <file>"
               << std::endl;
 }
 
@@ -84,6 +87,14 @@ int main(int argc, char **argv) {
             int stacks = stringToInt(args.at(5));
             std::string file = args.at(6);
             // generateCone(radius,height,slices,stacks,file)
+        } else if (args.at(1) == "cylinder") {
+            float radius = stringToDouble(args.at(2));
+            float height = stringToDouble(args.at(3));
+            int slices = stringToInt(args.at(4));
+            int stacks = stringToInt(args.at(5));
+            std::string file = args.at(6);
+            figures::Cylinder cylinder(radius, height, slices, stacks);
+            cylinder.writeToFile(file);
         } else {
             printUsage(args[0]);
         }
