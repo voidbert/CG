cc2d3348f15aeb361fe0f5f3d86add1591540543
Mariana <108686064+Mariana-rc01@users.noreply.github.com>
2025-02-26 23:33:56 +0000

    Remove repeated vertices from sphere (#14)
    * No repeated vertices
    
    * Remove even more vertices from sphere
    
    ---------
    
    Co-authored-by: Voidbert <humbertogilgomes@protonmail.com>

--- src/generator/figures/Sphere.cpp
+++ src/generator/figures/Sphere.cpp
@@ -22,12 +22,13 @@ Sphere::Sphere(float radius, int slices, int stacks) {
     float stackStep = M_PI / stacks;
     float sliceStep = 2 * M_PI / slices;
 
-    for (int iStack = 0; iStack <= stacks; iStack++) {
+    this->positions.push_back(glm::vec4(0.0f, radius, 0.0f, 1.0f));
+    for (int iStack = 1; iStack < stacks; iStack++) {
         float theta = iStack * stackStep;
         float y = radius * cosf(theta);
         float xz = radius * sinf(theta);
 
-        for (int jSlice = 0; jSlice <= slices; jSlice++) {
+        for (int jSlice = 0; jSlice < slices; jSlice++) {
             float phi = jSlice * sliceStep;
             float x = xz * sinf(phi);
             float z = xz * cosf(phi);
@@ -36,14 +37,34 @@ Sphere::Sphere(float radius, int slices, int stacks) {
         }
     }
 
-    for (int iStack = 0; iStack < stacks; iStack++) {
-        for (int jSlice = 0; jSlice < slices; jSlice++) {
-            int curr = iStack * (slices + 1) + jSlice;
-            int next = (iStack + 1) * (slices + 1) + jSlice;
+    int southPoleIndex = this->positions.size();
+    this->positions.push_back(glm::vec4(0.0f, -radius, 0.0f, 1.0f));
 
-            this->faces.push_back(utils::TriangleFace(curr, next, next + 1));
-            this->faces.push_back(utils::TriangleFace(curr, curr + 1, next + 1));
+    for (int jSlice = 0; jSlice < slices; jSlice++) {
+        int nextSlice = (jSlice + 1) % slices + 1;
+        this->faces.push_back(utils::TriangleFace(0, jSlice + 1, nextSlice));
+    }
+
+    for (int iStack = 0; iStack < stacks - 2; ++iStack) {
+        int topStart = 1 + iStack * slices;
+        int bottomStart = 1 + (iStack + 1) * slices;
+
+        for (int jSlice = 0; jSlice < slices; ++jSlice) {
+            int currentTop = topStart + jSlice;
+            int nextTop = topStart + (jSlice + 1) % slices;
+            int currentBottom = bottomStart + jSlice;
+            int nextBottom = bottomStart + (jSlice + 1) % slices;
+
+            faces.push_back(utils::TriangleFace(currentTop, currentBottom, nextBottom));
+            faces.push_back(utils::TriangleFace(currentTop, nextBottom, nextTop));
         }
+    }
+
+    int bottom = 1 + (stacks - 2) * slices;
+    for (int jSlice = 0; jSlice < slices; jSlice++) {
+        int current = bottom + jSlice;
+        int next = bottom + (jSlice + 1) % slices;
+        this->faces.push_back(utils::TriangleFace(southPoleIndex, next, current));
     }
 }
 
