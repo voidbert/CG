c661fb15d9e528fc5687eb7804b569048733bf28
Sara Lopes <92881231+saraazevedolopes@users.noreply.github.com>
2025-03-10 22:35:55 +0000

    Free Camera (#37)
    * Free Camera
    
    * Fix

--- src/engine/window/SceneWindow.cpp
+++ src/engine/window/SceneWindow.cpp
@@ -18,6 +18,8 @@
 #include <map>
 
 #include "engine/camera/Camera.hpp"
+#include "engine/camera/FreeCamera.hpp"
+#include "engine/camera/OrbitalCamera.hpp"
 #include "engine/window/SceneWindow.hpp"
 
 namespace engine::window {
@@ -43,22 +45,44 @@ SceneWindow::SceneWindow(const std::string &sceneFile) :
 }
 
 void SceneWindow::onUpdate(float time, float timeElapsed) {
-    static_cast<void>(time); // Evita warning de parâmetro não usado
+    static_cast<void>(time);
 
     GLFWwindow *windowHandle = this->getHandle();
+    camera::Camera &baseCamera = scene.getCamera();
 
-    std::map<int, camera::MovementDirection> keyMapping = {
-        { GLFW_KEY_W, camera::MovementDirection::Up       },
-        { GLFW_KEY_S, camera::MovementDirection::Down     },
-        { GLFW_KEY_A, camera::MovementDirection::Left     },
-        { GLFW_KEY_D, camera::MovementDirection::Right    },
-        { GLFW_KEY_F, camera::MovementDirection::Forward  },
-        { GLFW_KEY_B, camera::MovementDirection::Backward }
-    };
+    std::map<int, camera::MovementDirection> keyMapping;
+
+    camera::OrbitalCamera *orbitalCamera = dynamic_cast<camera::OrbitalCamera *>(&baseCamera);
+    if (orbitalCamera) {
+        keyMapping = {
+            { GLFW_KEY_W, camera::MovementDirection::Up       },
+            { GLFW_KEY_S, camera::MovementDirection::Down     },
+            { GLFW_KEY_A, camera::MovementDirection::Left     },
+            { GLFW_KEY_D, camera::MovementDirection::Right    },
+            { GLFW_KEY_F, camera::MovementDirection::Forward  },
+            { GLFW_KEY_B, camera::MovementDirection::Backward }
+        };
+    }
+
+    camera::FreeCamera *freeCamera = dynamic_cast<camera::FreeCamera *>(&baseCamera);
+    if (freeCamera) {
+        keyMapping = {
+            { GLFW_KEY_W,          camera::MovementDirection::Forward   },
+            { GLFW_KEY_S,          camera::MovementDirection::Backward  },
+            { GLFW_KEY_A,          camera::MovementDirection::Left      },
+            { GLFW_KEY_D,          camera::MovementDirection::Right     },
+            { GLFW_KEY_SPACE,      camera::MovementDirection::Up        },
+            { GLFW_KEY_LEFT_SHIFT, camera::MovementDirection::Down      },
+            { GLFW_KEY_LEFT,       camera::MovementDirection::LookLeft  },
+            { GLFW_KEY_RIGHT,      camera::MovementDirection::LookRight },
+            { GLFW_KEY_UP,         camera::MovementDirection::LookUp    },
+            { GLFW_KEY_DOWN,       camera::MovementDirection::LookDown  }
+        };
+    }
 
     for (const auto &[key, direction] : keyMapping) {
         if (glfwGetKey(windowHandle, key) == GLFW_PRESS) {
-            scene.getCamera().move(direction, timeElapsed);
+            baseCamera.move(direction, timeElapsed);
         }
     }
 }
