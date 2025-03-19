11cce2e8581771388ddf4695de47ba6716393285
Mariana <108686064+Mariana-rc01@users.noreply.github.com>
2025-03-19 12:54:11 +0000

    Add UI (#42)
    * Add UI
    
    * fix format
    
    * Build ImGUI in the recommended way
    
    * Remove imgui.ini after .gitignore modification
    
    * Miscelaneous fixes
    
    * Change namespace of UI
    
    ---------
    
    Co-authored-by: voidbert <humbertogilgomes@protonmail.com>

--- include/engine/window/Window.hpp
+++ include/engine/window/Window.hpp
@@ -35,10 +35,9 @@ public:
 
     int getWidth() const;
     int getHeight() const;
-
-protected:
     GLFWwindow *getHandle();
 
+protected:
     virtual void onUpdate(float time, float timeElapsed) = 0;
     virtual void onRender() = 0;
     virtual void onResize(int _width, int _height) = 0;
