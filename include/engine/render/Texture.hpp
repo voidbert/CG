8c6d24b101cc2ce299869bb351d4bbad52892bde
Voidbert <humbertogilgomes@protonmail.com>
2025-05-08 21:48:11 +0100

    Add texture loading

---
+++ include/engine/render/Texture.hpp
@@ -0,0 +1,35 @@
+/// Copyright 2025 Ana Oliveira, Humberto Gomes, Mariana Rocha, Sara Lopes
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
+#pragma once
+
+#include <glad/glad.h>
+#include <string>
+
+namespace engine::render {
+
+class Texture {
+private:
+    GLuint tid;
+
+public:
+    explicit Texture(const std::string &path);
+    Texture(const Texture &texture) = delete;
+    Texture(Texture &&texture) = delete;
+    ~Texture();
+
+    void use();
+};
+
+}
