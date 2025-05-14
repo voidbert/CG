d428f75f433453fe2d67268ae58a2fc15dbc4a11
sara <saralopes3001@gmail.com>
2025-05-14 18:33:55 +0100

    Fix override error lint

--- include/engine/scene/camera/ThirdPersonCamera.hpp
+++ include/engine/scene/camera/ThirdPersonCamera.hpp
@@ -49,7 +49,7 @@ public:
                                      bool showNormals) const override;
     virtual int drawShadedParts(render::RenderPipelineManager &pipelineManager,
                                 bool fillPolygons) const override;
-    virtual int drawForPicking(render::RenderPipelineManager &pipelineManager, int currentId) const;
+    virtual int drawForPicking(render::RenderPipelineManager &pipelineManager, int currentId) const override;
 
 protected:
     virtual void updateWithMotion() override;
